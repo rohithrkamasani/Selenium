af379ce6950b445add522491e1053cfa28311990
rohithrkamasani <rohithkumarreddykamasani@gmail.com>
2024-07-12 12:04:11 +0530

    Updated

--- src/testNGDemo/DemoGitHub.java
+++ src/testNGDemo/DemoGitHub.java
@@ -1,6 +1,8 @@
 package testNGDemo;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class DemoGitHub {
