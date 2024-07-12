decfa722372c392749a291ae353eef4aca4049a5
rohithrkamasani <rohithkumarreddykamasani@gmail.com>
2024-07-12 12:16:03 +0530

    TestDemo

--- src/testNGDemo/HelloGitHub.java
+++ src/testNGDemo/HelloGitHub.java
@@ -2,18 +2,25 @@ package testNGDemo;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
 public class HelloGitHub {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void google() {
+	  driver.get("https://www.google.com");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 
   @AfterTest
   public void afterTest() {
+	  driver.close();
   }
 
 }
